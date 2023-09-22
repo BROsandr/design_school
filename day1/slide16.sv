ea1caeebec4645000d9c0c389148e82de95fd405
alexkharl <sasha_xarlamov@mail.ru>
2023-09-22 17:55:08 +0300

    Fix(day1):Замена на reg для слайда 16

--- day1/slide16.sv
+++ day1/slide16.sv
@@ -1,7 +1,7 @@
 module top (
-  input        clk,
-  input  [8:0] a,
-  input        b,
-  output [3:0] q // выходные сигналы и шины можно объявлять как reg
+  input            clk,
+  input      [8:0] a,
+  input            b,
+  output reg [3:0] q // выходные сигналы и шины можно объявлять как reg
 );
 endmodule
