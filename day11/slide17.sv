9a1448160a178e1e97b5a1a11cb7ee5c04e59760
brosandr <sasha_xarlamov@mail.ru>
2024-10-24 13:27:41 +0300

    Feat(day11):Доб-ие slide17

---
+++ day11/slide17.sv
@@ -0,0 +1,25 @@
+module fsm
+(... ports ...);
+import fsm_pkg::*;
+state_e state;
+
+always_ff @(posedge clk, negedge rst_n)
+    if (!rst_n) begin
+        state     <= IDLE;
+        <outputs> <= '0;
+    end
+    else begin
+        state <= XXX;
+        <outputs> <= '0;
+        case (state)
+            IDLE : if (go) begin
+                     rd <= '1;
+                   end
+                   else
+            READ : begin
+                     rd <= '1;
+                   end
+            ...
+        endcase
+    end
+endmodule
