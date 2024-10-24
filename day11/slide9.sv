886c097a6a06d834d82ea9edf9f9f571a06b5dcf
brosandr <sasha_xarlamov@mail.ru>
2024-10-24 12:31:44 +0300

    Feat(day11):Доб-ие slide9

---
+++ day11/slide9.sv
@@ -0,0 +1,13 @@
+typedef enum bit
+    {
+        S0 = 1'd0,
+        S1 = 1'd1
+    }
+    state_e;
+    state_e state, next_state;
+    // State register
+    always_ff @ (posedge clk or posedge rst)
+      if (rst)
+          state <= S0;
+      else if (en)
+          state <= next_state;
