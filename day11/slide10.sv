30661612419d6129b2c1e6cea2caf9d7f24cba45
brosandr <sasha_xarlamov@mail.ru>
2024-10-24 13:06:58 +0300

    Feat(day11):Испр-ие слайда 10

--- day11/slide10.sv
+++ day11/slide10.sv
@@ -1,10 +1,13 @@
 always_comb
     begin
         next_state = state;
+
         case (state)
-        S0: if (~ a) next_state = S1;
-        S1: if (  a) next_state = S0;
+        S0: if (~a) next_state = S1;
+        S1: if ( a) next_state = S0;
         endcase
     end
-    // Output logic based on current state and inputs
-    assign y = (a & state == S1);
+
+
+// Output logic based on current state and inputs
+assign y = (a & state == S1);
