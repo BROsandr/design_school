df27287229a9f32e2e9f474a717e85d666296753
brosandr <sasha_xarlamov@mail.ru>
2024-10-24 12:58:32 +0300

    Feat(day11):Изм-ие 6го слайда

--- day11/slide6.sv
+++ day11/slide6.sv
@@ -1,8 +1,8 @@
-shift_reg # (.depth (w_led)) i_shift_reg
-    (
-        .en      ( enable ),
-        .seq_in  ( |key   ),
-        .seq_out ( fsm_in ),
-        .par_out ( led    ),
-        .*
-    );
+shift_reg # (.depth (w_led)
+) i_shift_reg (
+  .en      ( enable ),
+  .seq_in  ( |key   ),
+  .seq_out ( fsm_in ),
+  .par_out ( led    ),
+  .*
+);
