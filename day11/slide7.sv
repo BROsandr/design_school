70d9c984848897021af7a9445304703817cf52bb
brosandr <sasha_xarlamov@mail.ru>
2024-10-24 13:01:12 +0300

    Feat(day11):Испр-ие слайда 7

--- day11/slide7.sv
+++ day11/slide7.sv
@@ -1,6 +1,6 @@
 snail_moore_fsm i_moore_fsm
         (.en (enable), .a (fsm_in), .y (moore_fsm_out), .*);
-     snail_mealy_fsm i_mealy_fsm
+snail_mealy_fsm i_mealy_fsm
         (.en (enable), .a (fsm_in), .y (mealy_fsm_out), .*);
 
     always_comb
